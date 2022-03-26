3dc6e248cf315a01da4759314ec81a6050ac9bb1
oakpine <58812390+oakpine@users.noreply.github.com>
2022-03-26 12:29:04 -0400

    Added sidenav for mobile and materialize js
    When on mobile, navbar collapses to sidenav. Also decreased header text size when screen is small.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.parallax').parallax(); // image parallax
+    $(".dropdown-trigger").dropdown({coverTrigger: false}); // navbar dropdown
+    $('.sidenav').sidenav();
+})
