fb2edfa0791cd1e7e9c1db72247f3d201d727a59
oakpine <58812390+oakpine@users.noreply.github.com>
2022-03-29 22:56:12 -0400

    Edited faqs to have collapsibles

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
     $('.parallax').parallax(); // image parallax
     $(".dropdown-trigger").dropdown({coverTrigger: false}); // navbar dropdown
     $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+
+    $( ".collapsible-header" ).click(function() {
+      $(".more",this).toggle()
+      $(".less", this).toggle()
+    });
 })
